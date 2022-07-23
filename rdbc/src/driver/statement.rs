3207c5616e03a90781b3c50656ae7945f98badf2
yayanyang <yayanyang@gmail.com>
2022-07-23 21:49:38 +0800

    update

--- rdbc/src/driver/statement.rs
+++ rdbc/src/driver/statement.rs
@@ -2,7 +2,10 @@ use crate::waker;
 use anyhow::*;
 
 pub type Execute = waker::WakableFuture<Result<ExecuteResult>>;
-pub type Query = waker::WakableFuture<Result<Rows>>;
+pub type Query = waker::WakableFuture<Result<Box<dyn Rows>>>;
+pub type Columns = waker::WakableFuture<Result<Vec<ColumnMetaData>>>;
+pub type RowsNext = waker::WakableFuture<Result<bool>>;
+pub type RowsGet = waker::WakableFuture<Result<Option<Value>>>;
 
 pub trait Statement {
     /// Returns the number of placeholder parameters.
@@ -19,8 +22,49 @@ pub trait Statement {
     fn query(&mut self, args: Vec<NamedValue>) -> Query;
 }
 
-pub struct NamedValue {}
+pub struct NamedValue {
+    pub name: String,
+    pub ordinal: u32,
+    pub value: Value,
+}
 
-pub struct ExecuteResult {}
+pub enum Value {
+    I8(i8),
+    I16(i16),
+    I32(i32),
+    I64(i64),
+    F32(f32),
+    F64(f64),
+    String(String),
+    Bytes(Vec<u8>),
+}
 
-pub struct Rows {}
+pub struct ExecuteResult {
+    pub last_insert_id: u64,
+    pub raws_affected: u64,
+}
+
+pub trait Rows {
+    fn colunms(&mut self) -> Columns;
+
+    fn next(&mut self) -> RowsNext;
+
+    fn get(&mut self, index: u64) -> RowsGet;
+}
+
+pub struct ColumnMetaData {
+    pub column_index: u64,
+    pub column_name: String,
+    pub column_type: ColumnType,
+}
+
+pub enum ColumnType {
+    I8,
+    I16,
+    I32,
+    I64,
+    F32,
+    F64,
+    String,
+    Bytes,
+}
