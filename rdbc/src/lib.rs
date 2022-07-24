9cf8cbf7db2b2a6be21541dc57c34eae913aa8a3
yayanyang <yayanyang@gmail.com>
2022-07-24 16:39:22 +0800

    add global datasource

--- rdbc/src/lib.rs
+++ rdbc/src/lib.rs
@@ -247,3 +247,32 @@ impl Rows {
         self.inner.get(index).await
     }
 }
+
+#[cfg(feature = "global-datasource")]
+mod global {
+    use super::*;
+
+    fn global_datasource() -> &'static mut DataSource {
+        static mut CONF: std::mem::MaybeUninit<DataSource> = std::mem::MaybeUninit::uninit();
+        static ONCE: std::sync::Once = std::sync::Once::new();
+        ONCE.call_once(|| unsafe {
+            CONF.as_mut_ptr().write(DataSource::new());
+        });
+        unsafe { &mut *CONF.as_mut_ptr() }
+    }
+
+    pub fn register_driver(name: &str, driver: impl driver::Driver + 'static) -> Result<()> {
+        global_datasource().register_driver(name, driver)
+    }
+
+    pub fn unregister_driver(name: &str) -> Result<()> {
+        global_datasource().unregister_driver(name)
+    }
+
+    pub fn open(name: &str, url: &str) -> Result<Database> {
+        global_datasource().open(name, url)
+    }
+}
+
+#[cfg(feature = "global-datasource")]
+pub use global::*;
