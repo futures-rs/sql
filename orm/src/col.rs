a93984ac01452799ad844a37631765defd3bf60d
yayanyang <yayanyang@gmail.com>
2022-08-10 19:16:59 +0800

    update

--- orm/src/col.rs
+++ orm/src/col.rs
@@ -20,6 +20,33 @@ pub trait Deserialize {
         D: Deserializer;
 }
 
+impl<T> Serialize for Option<T>
+where
+    T: Serialize + Default,
+{
+    fn orm_seralize<S>(&mut self, ser: &mut S) -> anyhow::Result<()>
+    where
+        S: Serializer,
+    {
+        if let Some(t) = self.as_mut() {
+            return t.orm_seralize(ser);
+        }
+
+        Ok(())
+    }
+}
+
+impl<T> Deserialize for Option<T>
+where
+    T: Deserialize + Default,
+{
+    fn orm_seralize<S>(&mut self, ser: &mut S) -> anyhow::Result<()>
+    where
+        S: Serializer,
+    {
+    }
+}
+
 #[derive(Debug, Default)]
 pub struct Column<Data> {
     _data: Option<Data>,
@@ -29,13 +56,13 @@ pub struct Column<Data> {
 
 impl<Data> Serialize for Column<Data>
 where
-    Data: ColumnValue<ColumnType = Data> + Default,
+    Data: Serialize + Deserialize + Default,
 {
-    fn orm_seralize<S>(&mut self, _ser: &mut S) -> anyhow::Result<()>
+    fn orm_seralize<S>(&mut self, ser: &mut S) -> anyhow::Result<()>
     where
         S: Serializer,
     {
-        self._data.cast_to_rdbc_value()?;
+        self._data.orm_seralize(ser)?;
 
         Ok(())
     }
@@ -45,10 +72,12 @@ impl<Data> Deserialize for Column<Data>
 where
     Data: ColumnValue<ColumnType = Data> + Default,
 {
-    fn orm_deseralize<D>(&mut self, _der: &mut D) -> anyhow::Result<()>
+    fn orm_deseralize<D>(&mut self, der: &mut D) -> anyhow::Result<()>
     where
         D: Deserializer,
     {
+        self._data.orm_deseralize(der)?;
+
         Ok(())
     }
 }
